8f25b9fb4c3568e789de0814665dcad0ff0b133d
hvferreira <heldervferreira@gmail.com>
2023-03-11 18:42:35 +0000

    create enum rankings

--- src/main/java/org/example/Ranking.java
+++ src/main/java/org/example/Ranking.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public enum Ranking {
+
+    Straightflush,Fourofakind,FullHouse,Flush,Straight,ThreeofaKind,TwoPairs,Pair,HighCard;
+
+    private static final Ranking[] rankings= Ranking.values();
+
+    public static Ranking[] getAllRanking(){
+
+        return rankings;
+    }
 }
